aa796c7c24ca4f1e0acd9368122c705c42f19752
damianjnc <38845039+damianjnc@users.noreply.github.com>
2019-04-08 05:35:51 +0100

    simple redux app finish

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -8,7 +8,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              onClick={() => this.props.selectSong(song)}
+              className="ui button primary"
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -22,6 +27,7 @@ class SongList extends Component {
 }
 
 const mapStateToProps = state => {
+  console.log(state);
   return state;
 };
 
